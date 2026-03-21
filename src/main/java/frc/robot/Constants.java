afff18ea890b75f916c62faa6e2e4f4a57f32f81
3robin <erobin@aimpa.org>
2026-03-21 15:08:49 -0400

    testing shooter pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,7 @@ public final class Constants
     public static final double SHOOTER_FLYWHEEL_VOLTAGE = 5.8; //Sets speed
     public static final double voltage = 1;
 
-    public static final double SHOOTER_FLYWHEEL_VELOCITY = 100; 
+    public static final double SHOOTER_FLYWHEEL_VELOCITY = 3000; 
 
     // setting velocity constants
     public static final double FLYWHEEL_MOTOR_P = 0.001;
